78ad6ea33311cb07286fb4561179c4c6ce282e7f
Daniil Gilmutdinov <fregire@mail.ru>
2023-05-09 16:57:52 +0500

    Restarting game and lost screen

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public Tower[] Towers;
+
+    private bool towersDestroyed;
+
+    public Transform LostPopup;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (towersDestroyed && !LostPopup.gameObject.activeSelf)
+        {   
+            LostPopup.gameObject.SetActive(true);
+        }
+        else
+        {
+            if (Towers.All((tower) => !tower.isActiveAndEnabled))
+            {
+                towersDestroyed = true;
+            }   
+        }
+    }
+
+    public void RestartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+}
